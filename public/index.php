0d30dea3c77ff5781ae06994c0ea4588fbc92e94
BlanchardIlela <blancilela@gmail.com>
2026-03-09 22:52:34 +0100

    Mes routes je viens de définir mes deux routes une pour index en utilisant le /blog comme chemin pour aller dans index.php et autre pour la catégorie en utilisant /blog/category comme chemin pour aller dans show.php

--- public/index.php
+++ public/index.php
@@ -2,4 +2,23 @@
 require '../vendor/autoload.php';
 
 $router = new AltoRouter();
+
+define('VIEW_PATH', dirname(__DIR__) . '/views');
+
+$router->map('GET', '/blog', function () {
+    require VIEW_PATH . '/post/index.php';
+});
+$router->map('GET', '/blog/category', function () {
+    require VIEW_PATH . '/category/show.php';
+});
+
+$match = $router->match();
+//$match['target']();
+
+
+if ($match) {
+    call_user_func($match['target']);
+} else {
+    echo "404 - Page not found";
+}
 ?>
