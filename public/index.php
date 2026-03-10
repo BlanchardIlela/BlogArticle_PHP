65df862c5e43c7224b31e9fdbc4fd6e73ee46065
BlanchardIlela <blancilela@gmail.com>
2026-03-10 15:14:17 +0100

    La réogarnisation de mes chemins en utilisant la class Router de namespace App et les méthodes get et run

--- public/index.php
+++ public/index.php
@@ -1,24 +1,11 @@
-<?php 
+<?php
 require '../vendor/autoload.php';
 
-$router = new AltoRouter();
+use App\Router;
 
-define('VIEW_PATH', dirname(__DIR__) . '/views');
-
-$router->map('GET', '/blog', function () {
-    require VIEW_PATH . '/post/index.php';
-});
-$router->map('GET', '/blog/category', function () {
-    require VIEW_PATH . '/category/show.php';
-});
-
-$match = $router->match();
-//$match['target']();
-
-
-if ($match) {
-    call_user_func($match['target']);
-} else {
-    echo "404 - Page not found";
-}
+$router = new App\Router(dirname(__DIR__) . '/views');
+$router
+    ->get('/blog', 'post/index', 'blog')
+    ->get('/blog/category', 'category/show', 'category')
+    ->run();
 ?>
