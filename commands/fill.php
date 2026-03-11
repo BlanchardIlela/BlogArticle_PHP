f88940f6783d10f4ddb498cc71f8d42fe725a44b
BlanchardIlela <blancilela@gmail.com>
2026-03-11 14:23:17 +0100

    J'ai supprime dans ma BDD

--- commands/fill.php
+++ commands/fill.php
@@ -2,3 +2,8 @@
 $pdo = new PDO('mysql:dbname=tutoblog;host=localhost', 'root', 'root', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
+
+$pdo->exec('TRUCATE TABLE post_category');
+$pdo->exec('TRUCATE TABLE post');
+$pdo->exec('TRUCATE TABLE category');
+$pdo->exec('TRUCATE TABLE user');
