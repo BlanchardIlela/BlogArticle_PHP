d1b54840f27a6f6e22da83576b87d1efc431193b
BlanchardIlela <blancilela@gmail.com>
2026-03-12 06:26:47 +0100

    Afficher les posts comme des objets

--- views/post/index.php
+++ views/post/index.php
@@ -8,3 +8,19 @@ $posts = $query->fetchAll(PDO::FETCH_OBJ);
 ?>
 
 <h1>Mon blog</h1>
+
+<div class="row">
+    <?php foreach($posts as $post): ?>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"><?= htmlentities($post->name) ?></h5>
+                    <p><?= nl2br(htmlentities($post->content)) ?></p>
+                    <p>
+                        <a href="#" class="btn btn-primary">Voir plus</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    <?php endforeach ?>
+</div>
