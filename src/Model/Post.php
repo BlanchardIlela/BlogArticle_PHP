859efc655530d1bb82ccf3bff628ac2427bcd10b
BlanchardIlela <blancilela@gmail.com>
2026-03-12 07:16:48 +0100

    Création de deux getteur getName pour afficher le nom et getExcerpt pour afficher le contenu

--- src/Model/Post.php
+++ src/Model/Post.php
@@ -1,16 +1,39 @@
 <?php
 namespace App\Model;
 
+use App\Helpers\Text;
+use DateTime;
+
 class Post {
 
     private $id;
 
     private $name;
 
+    private $slug;
+
     private $content;
 
     private $created_at;
 
     private $categories = [];
+
+    public function getName (): ?string
+    {
+        return $this->name;
+    }
+
+    public function getExcerpt (): ?string
+    {
+        if ($this->content === null) {
+            return null;
+        }
+        return nl2br(htmlentities(Text::excerpt($this->content, 60)));
+    }
+
+    public function getCreatedAt (): DateTime
+    {
+        return new DateTime($this->created_at);
+    }
 
 }
