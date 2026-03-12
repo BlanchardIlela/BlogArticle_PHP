7da3d3e6cec8ea4954e266844916614805808e53
BlanchardIlela <blancilela@gmail.com>
2026-03-12 06:15:50 +0100

    La requête de recuperation je récupère mes posts par ordre de date de création decroissant et en limitant par 12 lors d'affichage

--- views/post/index.php
+++ views/post/index.php
@@ -1,5 +1,10 @@
 <?php 
 $title = 'Mon blog';
+$pdo = new PDO('mysql:dbname=tutoblog;host=localhost', 'root', 'root', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+$query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT 12');
+$posts = $query->fetchAll(PDO::FETCH_OBJ);
 ?>
 
 <h1>Mon blog</h1>
