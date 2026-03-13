fad124a7098021d2f2fc2933795f615c87e70ed1
BlanchardIlela <blancilela@gmail.com>
2026-03-13 12:44:00 +0100

    Afficher le deux boutons précédent suivant

--- views/post/index.php
+++ views/post/index.php
@@ -7,8 +7,21 @@ $title = 'Mon blog';
 $pdo = new PDO('mysql:dbname=tutoblog;host=localhost', 'root', 'root', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
+
+$page = $_GET['page'] ?? 1;
+
+if (!filter_var($page, FILTER_VALIDATE_INT)) {
+    throw new Exception('Numéro de page invalide');
+}
+
+if ($page === '1') {
+    header('Location: ' . $router->url('home'));
+    http_response_code(301);
+    exit();
+}
+
 //Numéro de page courante
-$currentPage = (int)($_GET['page'] ?? 1);
+$currentPage = (int)$page;
 if ($currentPage <= 0) {
     throw new Exception('Numéro de page invalide');
 }
@@ -33,3 +46,20 @@ $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
         </div>
     <?php endforeach ?>
 </div>
+
+
+<div class="d-flex justify-content-between my-4">
+    <?php if($currentPage > 1): ?>
+        <?php 
+        $link = $router->url('home');
+        if ($currentPage > 2) $link .= '?page' . ($currentPage - 1)
+        ?>
+        <a href="<?= $link ?>?page=<?= $currentPage - 1 ?>" class="btn btn-primary">&laquo; Page précédente</a>
+    <?php endif ?>
+    <?php if($currentPage < $pages): ?>
+        <a href="<?= $router->url('home') ?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto">Page suivante &raquo;</a>
+    <?php endif ?>
+</div>
+
+
+
