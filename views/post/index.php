9ebfa7d7117519a21703c10f7bfb287e78729e68
BlanchardIlela <blancilela@gmail.com>
2026-03-13 12:02:28 +0100

    La pagination nous comptons tous les ids depuis la table post création de la varible $currentPage pour voir la page courante la variable $perPage définit la limite de page à 12

--- views/post/index.php
+++ views/post/index.php
@@ -7,7 +7,19 @@ $title = 'Mon blog';
 $pdo = new PDO('mysql:dbname=tutoblog;host=localhost', 'root', 'root', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
-$query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT 12');
+//Numéro de page courante
+$currentPage = (int)($_GET['page'] ?? 1);
+if ($currentPage <= 0) {
+    throw new Exception('Numéro de page invalide');
+}
+//Compter les ids
+$count = (int)$pdo->query('SELECT COUNT(id) FROM post')->fetch(PDO::FETCH_NUM)[0];
+$perPage = 12;
+$pages = ceil($count / $perPage);
+if ($currentPage > $pages) {
+    throw new Exception('Cette page n\'existe pas');
+}
+$query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT ' . $perPage);
 $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 ?>
 
