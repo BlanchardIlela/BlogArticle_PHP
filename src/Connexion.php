bb8bc494ac7ed99d01ad8bfa5746cc3fd4c01588
BlanchardIlela <blancilela@gmail.com>
2026-03-15 09:05:30 +0100

    Nouvelle classe de connexion

--- src/Connexion.php
+++ src/Connexion.php
@@ -1,8 +1,15 @@
 <?php
 namespace App;
 
+use PDO;
+
 class Connexion {
 
-    
+    public static function getPDO (): PDO
+    {
+        return new PDO('mysql:dbname=tutoblog;host=localhost', 'root', 'root', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+    }
 
 }
