e9dcff45c26fafe4808785eeea9a3d6f32786e26
BlanchardIlela <blancilela@gmail.com>
2026-03-20 21:07:21 +0100

    paginatedQuery pour la category

--- views/category/show.php
+++ views/category/show.php
@@ -3,7 +3,7 @@
 use App\Connexion;
 use App\Model\Category;
 use App\Model\Post;
-use App\URL;
+use App\paginatedQuery;
 
 $id = (int)$params['id'];
 $slug = $params['slug'];
@@ -24,27 +24,18 @@ if($category->getSlug() !== $slug) {
     header('Location: ' .$url);
 }
 
-//Numéro de page courante
-$currentPage = URL::getPositiveInt('page', 1);
-//Compter les ids
-$count = (int)$pdo
-    ->query('SELECT COUNT(category_id) FROM post_category WHERE category_id = ' .$category->getID())
-    ->fetch(PDO::FETCH_NUM)[0];
-$perPage = 12;
-$pages = ceil($count / $perPage);
-if ($currentPage > $pages) {
-    throw new Exception('Cette page n\'existe pas');
-}
-$offset = $perPage * ($currentPage - 1); 
-$query = $pdo->query("
-    SELECT p.* 
+$paginationQuery = new paginatedQuery(
+    "SELECT p.* 
     FROM post p
     JOIN post_category pc ON pc.post_id = p.id 
     WHERE pc.category_id = {$category->getID()}
-    ORDER BY created_at DESC 
-    LIMIT $perPage OFFSET $offset
-");
-$posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
+    ORDER BY created_at DESC",
+    "SELECT COUNT(category_id) FROM post_category WHERE category_id = {$category->getID()}"
+);
+/**
+ * @avar Post[]
+ */
+$posts = $paginationQuery->getItems(Post::class);
 $link = $router->url('category', ['id' => $category->getID(), 'slug' => $category->getSlug()])
 ?>
 
@@ -59,15 +50,7 @@ $link = $router->url('category', ['id' => $category->getID(), 'slug' => $categor
 </div>
 
 <div class="d-flex justify-content-between my-4">
-    <?php if($currentPage > 1): ?>
-        <?php 
-        $l = $link;
-        if ($currentPage > 2) $l = $link . '?page' . ($currentPage - 1)
-        ?>
-        <a href="<?= $l ?>?page=<?= $currentPage - 1 ?>" class="btn btn-primary">&laquo; Page précédente</a>
-    <?php endif ?>
-    <?php if($currentPage < $pages): ?>
-        <a href="<?= $link ?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto">Page suivante &raquo;</a>
-    <?php endif ?>
+   <?= $paginationQuery->previousLink($link) ?>
+   <?= $paginationQuery->nextLink($link) ?>
 </div>
 
