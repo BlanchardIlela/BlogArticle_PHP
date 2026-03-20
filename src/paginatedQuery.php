3bca83153019d2a2995b49f32c5edc658123959a
BlanchardIlela <blancilela@gmail.com>
2026-03-20 21:40:16 +0100

    Les méthodes suivante : - previousLink : permet de gérer la page précédente; - nextLink : perPlease enter the commit message for your changes. Lines starting

--- src/paginatedQuery.php
+++ src/paginatedQuery.php
@@ -9,40 +9,75 @@ class paginatedQuery {
 
     private $query;
     private $queryCount;
-    private $classMapping;
     private $pdo;
     private $perPage;
+    private $count;
+    private $items;
 
     public function __construct(
         string $query,
         string $queryCount,
-        string $classMapping,
         ?PDO $pdo = null,
         int $perPage = 12
     )
     {
         $this->query = $query;
         $this->queryCount = $queryCount;
-        $this->classMapping = $classMapping;
         $this->pdo = $pdo ?: Connexion::getPDO();
         $this->perPage = $perPage;
     }
 
-    public function getItems(): array
+    public function getItems($classMapping): array
     {
-        $currentPage = URL::getPositiveInt('page', 1);
-        $count = (int)$this->pdo
-            ->query($this->queryCount)
-            ->fetch(PDO::FETCH_NUM)[0];
-        $pages = ceil($count / $this->perPage);
-        if ($currentPage > $pages) {
-            throw new Exception('Cette page n\'existe pas');
-        }
-        $offset = $this->perPage * ($currentPage - 1); 
-        return $this->pdo->query(
-            $this->query .
-            " LIMIT {$this->perPage} OFFSET $offset")
-            ->fetchAll(PDO::FETCH_CLASS, $this->classMapping);
+        if ($this->items === null) {
+            $currentPage = $this->getCurrentPage();
+            $pages = $this->getPages();
+            if ($currentPage > $pages) {
+                throw new Exception('Cette page n\'existe pas');
+            }
+            $offset = $this->perPage * ($currentPage - 1); 
+            $this->items = $this->pdo->query(
+                $this->query .
+                " LIMIT {$this->perPage} OFFSET $offset")
+                ->fetchAll(PDO::FETCH_CLASS, $classMapping);
+            }
+            return $this->items;
+    }
+
+    public function previousLink (string $link): ?string
+    {
+        $currentPage = $this->getCurrentPage();
+        if($currentPage <= 1) return null;
+        if($currentPage > 2) $link .= "?page=" . ($currentPage - 1);
+        return <<<HTML
+        <a href="{$link}" class="btn btn-primary">&laquo; Page précédente</a>
+HTML;
+    }
+
+      public function nextLink (string $link): ?string
+    {
+        $currentPage = $this->getCurrentPage();
+        $pages = $this->getPages();
+        if($currentPage >= $pages) return null;
+        $link .= "?page=" . ($currentPage + 1);
+        return <<<HTML
+        <a href="{$link}" class="btn btn-primary">Page suivante &raquo;</a>
+HTML;
+    }
+
+    private function getCurrentPage(): int
+    {
+        return URL::getPositiveInt('page', 1);
+    }
+
+    private function getPages (): int
+    {
+        if ($this->count === null) {
+            $this->count = (int)$this->pdo
+                ->query($this->queryCount)
+                ->fetch(PDO::FETCH_NUM)[0];
+        } 
+        return ceil($this->count / $this->perPage);
     }
 
 }
