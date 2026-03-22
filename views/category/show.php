1251d6c2b10aa1c8a03b16c5a32fae642de60667
BlanchardIlela <blancilela@gmail.com>
2026-03-22 09:30:25 +0100

    requête de liaison

--- views/category/show.php
+++ views/category/show.php
@@ -36,6 +36,22 @@ $paginationQuery = new paginatedQuery(
  * @avar Post[]
  */
 $posts = $paginationQuery->getItems(Post::class);
+$postsByID = [];
+foreach ($posts as $post) {
+    $postsByID[$post->getID()] = $post;
+}
+$categories = $pdo
+    ->query('SELECT c.*, pc.post_id
+             FROM post_category pc
+             JOIN category c ON c.id = pc.category_id
+             WHERE pc.post_id IN (' . implode(',', array_keys($postsByID)) . ')'
+            )->fetchAll(PDO::FETCH_CLASS, Category::class);
+// On parcours les catégories
+foreach ($categories as $category) {
+    // On trouve l'article $posts correspondant à ligne
+    // On trouve la catégorie à l'article
+    $postsByID[$category->getPostID()]->addCategory($category);
+}
 $link = $router->url('category', ['id' => $category->getID(), 'slug' => $category->getSlug()])
 ?>
 
