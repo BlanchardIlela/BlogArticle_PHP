4de5f7c1f4f6c8e6b9e31038cbed40836bf61495
BlanchardIlela <blancilela@gmail.com>
2026-03-23 11:11:19 +0100

    Facilitation d'affichage avec la méthode findPaginated

--- views/post/index.php
+++ views/post/index.php
@@ -1,36 +1,14 @@
 <?php
 
 use App\Connexion;
-use App\Helpers\Text;
-use App\Model\Category;
-use App\Model\Post;
-use App\paginatedQuery;
-use App\URL;
+use App\Table\PostTable;
 
 $title = 'Mon blog';
 $pdo = Connexion::getPDO();
 
-$paginationQuery = new paginatedQuery(
-    "SELECT * FROM post ORDER BY created_at DESC",
-    "SELECT COUNT(id) FROM post"
-);
-$posts = $paginationQuery->getItems(Post::class);
-$postsByID = [];
-foreach ($posts as $post) {
-    $postsByID[$post->getID()] = $post;
-}
-$categories = $pdo
-    ->query('SELECT c.*, pc.post_id
-             FROM post_category pc
-             JOIN category c ON c.id = pc.category_id
-             WHERE pc.post_id IN (' . implode(',', array_keys($postsByID)) . ')'
-            )->fetchAll(PDO::FETCH_CLASS, Category::class);
-// On parcours les catégories
-foreach ($categories as $category) {
-    // On trouve l'article $posts correspondant à ligne
-    // On trouve la catégorie à l'article
-    $postsByID[$category->getPostID()]->addCategory($category);
-}
+$table = new PostTable($pdo);
+[$posts, $pagination] = $table->findPaginated();
+
 $link = $router->url('home');
 ?>
 
@@ -46,8 +24,8 @@ $link = $router->url('home');
 
 
 <div class="d-flex justify-content-between my-4">
-    <?= $paginationQuery->previousLink($link) ?>
-    <?= $paginationQuery->nextLink($link) ?>
+    <?= $pagination->previousLink($link) ?>
+    <?= $pagination->nextLink($link) ?>
 </div>
 
 
