e707544dc74924e2baae9990f67901d2eec380e0
BlanchardIlela <blancilela@gmail.com>
2026-03-24 15:54:48 +0100

    La table permettant d'afficher mes posts et la pagination également

--- views/admin/post/index.php
+++ views/admin/post/index.php
@@ -5,5 +5,30 @@ use App\Table\PostTable;
 
 $title = "Administration";
 $pdo = Connexion::getPDO();
-$posts = (new PostTable($pdo))->findPaginated();
+$link = $router->url('admin_posts');
+[$posts, $pagination] = (new PostTable($pdo))->findPaginated();
+
 ?>
+
+
+<table class="table">
+    <thead>
+        <th>Titre</th>
+        <th>Actions</th>
+    </thead>
+    <tbody>
+        <?php foreach($posts as $post): ?>
+            <tr>
+                <td><?= htmlentities($post->getName()) ?></td>
+                <td>
+
+                </td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
+
+<div class="d-flex justify-content-between my-4">
+    <?= $pagination->previousLink($link) ?>
+    <?= $pagination->nextLink($link) ?>
+</div>
