095f8bbed4ab1d284f3915bd095399a92c51890a
BlanchardIlela <blancilela@gmail.com>
2026-03-24 16:00:13 +0100

    Deux routes de plus - admin_post pour l'edit d'un post - admin_post_new pour un ajout d'un nouvel article

--- public/index.php
+++ public/index.php
@@ -28,5 +28,7 @@ $router
     ->get('/blog/category/[*:slug]-[i:id]', 'category/show', 'category')
     ->get('/blog/[*:slug]-[i:id]', 'post/show', 'post')
     ->get('/admin', 'admin/post/index', 'admin_posts')
+    ->get('/admin/post/[i:id]', 'admin/post/edit', 'admin_post')
+    ->get('/admin/post/new', 'admin/post/new', 'admin_post_new')
     ->run();
 ?>
