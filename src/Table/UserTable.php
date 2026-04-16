f61d5e56a93057e28707795a15849d2b24c5ebd1
BlanchardIlela <blancilela@gmail.com>
2026-04-16 05:57:32 +0100

    Vérification de l'existence de l'utilisateur

---
+++ src/Table/UserTable.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Table;
+
+use App\Model\User;
+use App\Table\Exception\NotFoundExcept;
+use PDO;
+
+class UserTable extends Table{
+
+    protected $table = "user";
+    protected $class = User::class;
+
+    public function findByUsername(string $username)
+    {
+        $query = $this->pdo->prepare('SELECT * FROM ' .$this->table .' WHERE username = :username');
+        $query->execute(['username' => $username]);
+        $query->setFetchMode(PDO::FETCH_CLASS, $this->class);
+        $result = $query->fetch();
+        if ($result === false) {
+            throw new NotFoundExcept($this->table, $username);
+            
+        }
+        return $result;
+    }
+
+}
