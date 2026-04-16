0c9e273d20c35d4b6d0572078034ad49cfd4003c
BlanchardIlela <blancilela@gmail.com>
2026-04-16 05:59:09 +0100

    Ouverture de la session

--- src/Auth.php
+++ src/Auth.php
@@ -1,12 +1,18 @@
 <?php
 namespace App;
 
+use App\Security\ForbiddenExcept;
 
 class Auth {
 
     public static function check ()
     {
-        
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        if (!isset($_SESSION['auth'])) {
+            throw new ForbiddenExcept();
+        }
     }
 
 }
